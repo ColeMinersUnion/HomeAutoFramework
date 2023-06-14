77235bc0e4df6d88c93c55058c6a9e81ff237865
Cole Hansen <130320410+ColeMinersUnion@users.noreply.github.com>
2023-06-14 17:02:29 -0400

    Update WServe.py

--- WServe.py
+++ WServe.py
@@ -1,11 +1,18 @@
 #Websocket Echo Server
 import asyncio
+import json
 from websockets.server import serve
+from WStrucks import Connections
+
+con = Connections()
+
+async def initWS(websocket, event):
+    con.addController(event{"SenderID"})
 
 
 async def handler(websocket):
-    async for message in websocket:
-        
+    message = await websocket.recv()
+    event = json.loads(message)
 
 async def main():
     async with serve(handler, '10.4.50.240', 8000):
