276d592efa95f1df89e14e99b6927ab6270d7987
Cole Hansen <130320410+ColeMinersUnion@users.noreply.github.com>
2023-06-15 12:40:59 -0400

    Update WSclient.py

--- WSclient.py
+++ WSclient.py
@@ -2,7 +2,7 @@ import asyncio
 from websockets.sync.client import connect
 
 def hello():
-    with connect("ws://10.4.50.240:8000") as websocket:
+    with connect("ws://127.0.0.1:8000") as websocket:
         websocket.send("Hello world!")
         message = websocket.recv()
         print(f"Received: {message}")
