d78ea38d44114c357092be3de6481a3fba05675c
Cole Hansen <130320410+ColeMinersUnion@users.noreply.github.com>
2023-07-24 18:15:03 -0400

    Update WebsocketServer2.py
    Updated the server to handle forwarding client messages to a new client

--- WebsocketServer2.py
+++ WebsocketServer2.py
@@ -26,13 +26,42 @@ class MyServer:
                 
 
     async def Pong(self, websocket, message):
-        while ("PING" in message or "JOIN" in message):
+        
+        while ("PING" in message or "JOIN" in message or "MSGS" in message):
+
+            if("MSGS" in message):
+                id = message.replace("MSGS", "")
+                await self.Talk(id)
+
             time.sleep(2)
             await websocket.send("PONG")
             print("PONG")
             message = await websocket.recv()
-        print("HELP")
+        
+            
+
+    async def Talk(self, ID):
+        websocket = self.connections[ID]
+        message = "Hi me, this is a auto generated message."
+        print("About to send a message")
+        await websocket.send(message)
+        print("sent the message")
+        
 
 
 
 server = MyServer("localhost", 8080)
+
+
+""" TO-DO:
+
+Since each of the connections adds a new user to the connections dictionary,
+I could have the response to the join message relay the active users. 
+Actually I would rather receive a confirmation and send an update to all of the controller users of a new device. 
+
+Find a way to close connections without just forcibly breaking out of the connection.
+
+Integrate JSON as the way you organize data through communication.
+Strings are cool but it's unorganized
+"""
+
