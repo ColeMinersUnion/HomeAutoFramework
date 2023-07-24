86bef317141946b335911931fe9c3d7af8ffc034
Cole Hansen <130320410+ColeMinersUnion@users.noreply.github.com>
2023-07-24 13:46:31 -0400

    Rebuilt Websocket Client
    This is the other half of the new server I built. It's a lot easier when you know how asynchronous programming works.

---
+++ WebsocketClient2.py
@@ -0,0 +1,39 @@
+import asyncio
+import websockets
+import time
+
+class client:
+
+    def __init__(self, ID):
+        self.ID = ID
+        asyncio.run(self.JoinAndVibe())
+
+    def joinMSG(self):
+        return "JOIN" + self.ID
+
+    async def JoinAndVibe(self):
+        uri="ws://localhost:8080"
+
+        async with websockets.connect(uri) as websocket:
+
+            await websocket.send(self.joinMSG())
+            async for message in websocket:
+                await self.Response(websocket, message)
+                return "000"
+                
+
+    async def Response(self, websocket, message):
+        while("PONG" in message or "HEYO" in message):
+            time.sleep(2)
+            await websocket.send("PING")
+            print("PING")
+            message = await websocket.recv()
+
+
+        if("NONO" in message):
+            return "001"
+        
+    async def main(self):
+        await asyncio.gather(self.JoinAndVibe())
+            
+example = client("1234jkls")
