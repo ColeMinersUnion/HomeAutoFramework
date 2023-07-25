8febbd1b7c26c34701b73f3e30e6bd786eafd843
Cole Hansen <130320410+ColeMinersUnion@users.noreply.github.com>
2023-07-25 11:47:33 -0400

    Update WebsocketClientTwo.py
    I didn't need to write it all out twice. The client class now has everything it needs to send and receive commands

--- WebsocketClientTwo.py
+++ WebsocketClientTwo.py
@@ -1,41 +1,4 @@
-import asyncio
-import websockets
-import time
+from WebsocketClient2 import client
 
-class client:
-
-    def __init__(self, ID):
-        self.ID = ID
-        asyncio.run(self.JoinAndVibe())
-
-    def joinMSG(self):
-        return "JOIN" + self.ID
-
-    async def JoinAndVibe(self):
-        uri="ws://localhost:8080"
-
-        async with websockets.connect(uri) as websocket:
-
-            await websocket.send(self.joinMSG())
-            async for message in websocket:
-                await self.Response(websocket, message)
-                return "000"
-
-    async def Response(self, websocket, message):
-        while("PONG" in message or "HEYO" in message or "auto" in message):
-
-            if("auto" in message):
-                print("You received a new message!")
-
-            time.sleep(2)
-            await websocket.send("PING")
-            print("PING")
-            message = await websocket.recv()
-
-
-        
-        
-    async def main(self):
-        await asyncio.gather(self.JoinAndVibe())
-            
-example = client("1234jklq")
+another = client("2345jkla")
+another.start()
