ce18d9f0d9f8f01d528dbd53834ec4c334be6e5b
Cole Hansen <130320410+ColeMinersUnion@users.noreply.github.com>
2023-07-29 08:19:12 -0400

    Update WebsocketServer2.py
    We added colors to our output text. I also made a function to log what's happening in the server to help the debugging process.

--- WebsocketServer2.py
+++ WebsocketServer2.py
@@ -2,30 +2,35 @@ import websockets
 import asyncio
 import time
 import json
+from PrintColors import bcolors
 
 class MyServer:
 
-    def __init__(self, Domain, port) -> None:
+    def __init__(self, Domain, port, file="logs.txt") -> None:
         self.Domain = Domain
         self.port = port
+        self.file = file
         self.connections = {}
 
 
     def start(self):
-        print("About to start the server")
+        print(bcolors.WARNING + "About to start the server" + bcolors.ENDC)
+
+        with open(self.file, "w") as file:
+            file.writelines(bcolors.WARNING + "About to start the server" + bcolors.ENDC)
+
         self.server = websockets.serve(self.handlerFunc, self.Domain, self.port)
         asyncio.get_event_loop().run_until_complete(self.server)
         asyncio.get_event_loop().run_forever()
 
 
     async def handlerFunc(self, websocket):
-        print("The server is up and running")
+        self.logMsg(bcolors.OKGREEN + "The server is up and running" + bcolors.ENDC)
         async for message in websocket:
             message = json.loads(message)
             if(message["Type"] == "Join" and len(self.connections) < 25):
-                print("CONNECTION SUCCESSFUL")
+                self.logMsg(bcolors.OKGREEN + "CONNECTION SUCCESSFUL" + bcolors.ENDC)
                 self.connections[message["Message"]] = websocket
-                print("Got here!")
                 await self.Pong(websocket, message)
                 
 
@@ -33,11 +38,11 @@ class MyServer:
         
         others = self.connections.keys()
 
-        
+        self.logMsg(bcolors.BOLD + str(self.connections) + bcolors.ENDC)        
 
         await websocket.send(self.genMsg("Welcome", list(self.connections)[-1], msg=list(others)))
 
-        print("Got here just fine")
+        self.logMsg(bcolors.OKCYAN + "Got here just fine" + bcolors.ENDC)
 
         message = await websocket.recv()
         message = json.loads(message)
@@ -49,7 +54,7 @@ class MyServer:
 
             time.sleep(2)
             await websocket.send(self.genMsg("PONG", message["Sender"]))
-            print("PONG")
+            self.logMsg(bcolors.HEADER + "PONG" + bcolors.ENDC)
             message = await websocket.recv()
             message = json.loads(message)
 
@@ -59,18 +64,28 @@ class MyServer:
 
     async def Talk(self, msg):
         ID = msg["Recipient"]
+        if ID not in self.connections:
+            await websocket.send(self.genMsg("Command", msg["Sender"], "Selected User is not in the server"))
+            return
         websocket = self.connections[ID]
         message = msg["Message"]
-        print("About to send a message")
+        self.lohMsg("About to send a message")
         await websocket.send(self.genMsg("Command", ID, message, msg["Sender"]))
-        print("sent the message")
+        self.logMsg("sent the message")
 
     def genMsg(self, Tpe, Recvr, msg="PONG", Sendr="Server"):
         newMsg = {"Type": Tpe, "Sender": Sendr, "Recipient": Recvr, "Message": msg}
         return json.dumps(newMsg)
 
+    def logMsg(self, logstr):
+        print(logstr)
+        with open(self.file, "a") as file:
+            file.writelines(logstr + "\n")
+        
 
 
+
+print("\n" * 5)
 server = MyServer("localhost", 8080)
 server.start()
 
@@ -81,6 +96,8 @@ server.start()
 I also want to figure out how to send a receive messages to specific devices
 I should be able to increase the delay between ping-pongs because I'm sending messages in between.
 how do i rearrange the awaitable objects so messages have a higher urgency
+
+Keep a text logs of everything that happens
 
 Find a way to close connections without just forcibly breaking out of the connection.
 
