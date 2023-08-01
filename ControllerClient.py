d5c5f3bc505d0c6d3972d54e692622c7cbc50208
Cole Hansen <130320410+ColeMinersUnion@users.noreply.github.com>
2023-08-01 16:45:15 -0400

    Update ControllerClient.py
    I had to do a lot of changes to make this work with the tkinter but It should hopefully work now.

--- ControllerClient.py
+++ ControllerClient.py
@@ -1,38 +1,55 @@
-from ..WebsocketV2.WebsocketClient2 import client
+from WebsocketClient2 import client
 import json
 import asyncio
-from ..WebsocketV2.PrintColors import bcolors
+import websockets
+from PrintColors import bcolors
 
 class ControllerClient(client):
 
     def __init__(self, ID):
         super().__init__(ID)
         self.ws = None
+
+    def start(self):
+        asyncio.run(self.JoinAndVibe())
+
+    def end(self):
+        self.ws.close()
         
+    async def JoinAndVibe(self):
+        uri="ws://localhost:8080"
+
+        websocket = await websockets.connect(uri)
+
+        await websocket.send(self.genMsg("Join", "Server", msg=self.ID))
+        async for message in websocket:
+            self.ws = websocket
+
+            await self.Response(websocket, message)
+                
+
     async def Response(self, websocket, message):
-        self.ws = websocket
-        while True:
+        message = await websocket.recv()
+        message = json.loads(message)
 
-            message = json.loads(message)
+        if message["Type"] == "Welcome":
+            print(bcolors.OKBLUE + str(message) + bcolors.ENDC)
+            for i in message["Message"]:
+                self.others.append(i)
+            print("Connection Successful\n")
 
+            print(self.others)
+            print("\n")
 
-            if message["Type"] == "Welcome":
-                print(bcolors.OKBLUE + str(message) + bcolors.ENDC)
-                for i in message["Message"]:
-                    self.others.append(i)
-                print("Connection Successful\n")
+        elif message["Type"] == "New_User":
+            print("We got a new User")
+            self.others.append(message["Message"])
+            print(bcolors.WARNING + str(self.others) + bcolors.ENDC)
 
-                print(self.others)
-                print("\n")
+        await websocket.send(self.genMsg("PING", "Server"))
+        print(bcolors.HEADER + "PING" + bcolors.ENDC)
 
-            elif message["Type"] == "New_User":
-                print("We got a new User")
-                self.others.append(message["Message"])
-                print(bcolors.WARNING + str(self.others) + bcolors.ENDC)
-
-            await websocket.send(self.genMsg("PING", "Server"))
-            print(bcolors.HEADER + "PING" + bcolors.ENDC)
-            message = await websocket.recv()
+        
 
     async def Toggle(self, recvr):
         await self.ws.send(self.genMsg("Command", recvr, msg="Toggle"))
